1fbaee66d8d3b59446a79fb2ad5c775ce909e37d
Brian Rush <yankeh@gmail.com>
2017-05-05 12:40:53 -0700

    add front-end logic to display results and back end logic to count to given number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
 // Business Logic
+var total = 0;
+var firstArray = []
 
+var counter = function(num){
+  for (var index = 0; index < num; index += 1) {
+    total += 1;
+    // alert(total);
+    firstArray.push(total);
+    // alert(firstArray);
+  }
+}
 
 // User Interface Logic
 
 $(document).ready(function () {
-  $("form#english").submit(function (event) {
+  $("form#user-number").submit(function (event) {
     event.preventDefault();
+    $('#result').empty();
 
+    var userNum = parseInt($("#num").val());
+    // alert(userNum);
+    var result = counter(userNum);
+    $("span#result").text(firstArray);
   });
 });
